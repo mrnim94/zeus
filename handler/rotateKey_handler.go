269d46c0637f1a10eb4fac9c258a1f17ba89984a
mr nim <mrnim@nimtechnology.com>
2023-07-06 00:54:36 +0700

    refactor code

--- handler/rotateKey_handler.go
+++ handler/rotateKey_handler.go
@@ -2,71 +2,36 @@ package handler
 
 import (
 	"fmt"
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/iam"
 	"github.com/go-co-op/gocron"
 	"time"
 	"zeus/helper"
+	"zeus/helper/aws_cloud"
 	"zeus/log"
 	"zeus/model"
 )
 
-func HandlerCreateDeleteKey(region string) {
+type RotateKeyHandler struct {
+	AWSCloud aws_cloud.AWSCloud
+}
 
-	fmt.Println(region)
+func (rk *RotateKeyHandler) HandlerCreateDeleteKey() {
+
 	var cfg model.RotationKey
 	helper.LoadConfigFile(&cfg)
 
 	s := gocron.NewScheduler(time.UTC)
 
 	rotationTask := func(userName string) error {
-		sess, err := session.NewSession(&aws.Config{
-			Region: aws.String(region)},
-		)
+		accessKey, err := rk.AWSCloud.RetentionAWSKey(userName)
+
 		if err != nil {
 			log.Error("Error creating session: %v", err)
 			return err
 		}
-		// Create IAM service client
-		svc := iam.New(sess)
 
-		// Provide the username
-		//userName := "nimtechnology"
+		fmt.Println("New Access Key ID:", *accessKey.AccessKeyId)
+		fmt.Println("New Secret Access Key:", *accessKey.SecretAccessKey)
 
-		// Step 1: Create a new access key
-		newAccessKey, err := svc.CreateAccessKey(&iam.CreateAccessKeyInput{
-			UserName: aws.String(userName),
-		})
-		if err != nil {
-			log.Error("Error creating new access key: %v", err)
-			return err
-		}
-		fmt.Println("New Access Key ID:", *newAccessKey.AccessKey.AccessKeyId)
-		fmt.Println("New Secret Access Key:", *newAccessKey.AccessKey.SecretAccessKey)
-
-		// Step 2: List old access keys
-		oldAccessKeys, err := svc.ListAccessKeys(&iam.ListAccessKeysInput{
-			UserName: aws.String(userName),
-		})
-		if err != nil {
-			log.Error("Error listing access keys: %v", err)
-		}
-
-		// Step 3: Delete old access keys
-		for _, keyMetadata := range oldAccessKeys.AccessKeyMetadata {
-			if *keyMetadata.AccessKeyId != *newAccessKey.AccessKey.AccessKeyId {
-				_, err := svc.DeleteAccessKey(&iam.DeleteAccessKeyInput{
-					UserName:    aws.String(userName),
-					AccessKeyId: aws.String(*keyMetadata.AccessKeyId),
-				})
-				if err != nil {
-					log.Error("Error deleting access key %s: %v", *keyMetadata.AccessKeyId, err)
-				} else {
-					log.Info("Deleted old access key: %s\n", *keyMetadata.AccessKeyId)
-				}
-			}
-		}
 		return nil
 	}
 
